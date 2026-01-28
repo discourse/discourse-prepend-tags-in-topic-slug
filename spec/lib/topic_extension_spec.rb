ba567c982bc8d9a4609e9ed643fa101ddfa33d8e
Jarek Radosz <jradosz@gmail.com>
2026-01-28 12:51:42 +0100

    DEV: Update linting and add types (#38)
    * DEV: Update linting and add types
    
    * DEV: Update linting and add types

--- spec/lib/topic_extension_spec.rb
+++ spec/lib/topic_extension_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe DiscoursePrependTagsInTopicSlug::TopicExtension do
   before { SiteSetting.prepend_tags_in_topic_slug_tag_list = "tag1|tag2" }
 
